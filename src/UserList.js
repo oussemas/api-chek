828273687c6e7a6017735f037de03990c1210bd1
oussemas <sima_oussema@yahoo.fr>
2022-03-17 18:42:19 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import UserCard from './UserCard'
+
+const UserList = () => {
+    const [listUsers, setListUsers] = useState([])
+    
+    const fetchUsers = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(res => setListUsers(res.data))
+            .catch(err => console.log(err))
+    }
+    
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    
+
+
+  return (
+    <div className='row' >
+    {listUsers.map((el, i)=><UserCard user={el} key={i} />)}
+</div>
+)
+}
+
+export default UserList
